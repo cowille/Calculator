cd6fa76ec4a84181993c479f5f9af4f736dda152
corneliuswille <wille@fh-bingen.de>
2013-11-26 12:00:40 +0100

    Initial Start 2013

--- Calculate.java
+++ Calculate.java
@@ -1,41 +1,6 @@
 public class Calculate {
 
-//Änderung
-	public int mult(int x, int y) {
-		int p;
-		p = x * y;
-		if (p == 49)
-			p = 0;
-		return p;
-	}
-
-	public int div(int x, int y) {
-		return x * y;
-	}
 	public double add(double x, double y) {
-		return x - y;
-	}
-	public double sub(double x, double y) {
 		return x + y;
-	}
-	public int square(int i){
-		return i*i;
-	}
-        
-        public int mult(int x, int y) {
-		return x * y;
-	}
-	
-	public int reduplucate(int x) {
-		return x*2;
-	}
-	
-	public int modulo(int x, int y){
-		return x % y;
-	}
-	
-	public int potency(int x, int y){
-		
-		return Math.pot(x,y);
 	}
 }
