f0d4d9d158389a46fad1ce2c604588965b00854c
A. Renner <a.renner@fh-bingen.de>
2014-06-12 22:10:51 +0200

    Group Adrian Renner, Maximilian Buettner, Jonas Depoix and Meik Specht

---
+++ App.java
@@ -0,0 +1,13 @@
+// created by addyi
+
+public class App{
+	public static void main(String[] args) {
+		Calculate c = new Calculate();
+		
+		System.out.println("3,14 + 3.14 = "+c.add(3.14,3.14));
+		System.out.println("3,14 - 3.14 = "+c.sub(3.14,3.14));
+		System.out.println("3,14 * 3.14 = "+c.mult(3.14,3.14));
+		
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,6 @@
+Die Gruppe 
+	Adiran Renner
+	Maximilian Büttner
+	Jonas Depoix 
+	Meik Specht 
+hat die App.java geschrieben.
