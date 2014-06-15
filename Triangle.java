13f2e77ef1546b96bc3215f66867aecb9153686a
SKuhn84 <steffenkuhn84@gmx.de>
2014-06-15 11:45:29 +0200

    Methods for calculating the angle of a Triangle
    The results of all the methods are in radient.
    
    Steffen Kuhn 194643
    Sebastian Schamscha 197823
    Phillip Kuster 198122

---
+++ Triangle.java
@@ -0,0 +1,17 @@
+
+public class Triangle {
+
+	//Result in Radiant
+	public static double sin(double d){
+		return Math.sin(d);
+	}
+	public static double tan(double d){
+		return Math.tan(d);
+	}
+	public static double cos(double d){
+		return Math.cos(d);
+	}
+	public static double cot(double d){
+		return Math.atan(d);
+	}	
+}
