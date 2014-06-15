35da5874e7ce08528831feaa41b8241d2c4f3f2e
RitKow <dimitribabakow@googlemail.com>
2014-06-15 17:31:45 +0200

    Update Calculate.java
    Erweiterung squared und half
    Ritter, Babakow

--- Calculate.java
+++ Calculate.java
@@ -3,16 +3,26 @@ public class Calculate {
 	public double add(double x, double y) {
 		return x + y;
 	}
-	
+
 	public double sub(double x, double y) {
 		return x - y;
 	}
-	
+
 	public double mult(double x, double y) {
 		return x * y;
 	}
-	
+
 	public double div(double x, double y) {
 		return x / y;
 	}
+	
+	public double squared (double x) {
+		return x * x;
+	}
+	
+	public double half (double x){
+		return x / 2;
+	}
+	
+
 }
